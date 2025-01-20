6d883a24164dee33d547c201de45528673f6f38e
Gaby I <gabyi@Mac.attlocal.net>
2025-01-19 23:37:55 -0500

    edit config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,10 @@
 const { DateTime } = require('luxon');
 
 module.exports = function (eleventyConfig) {
-   eleventyConfig.addPassthroughCopy("./src/styles/");
-   eleventyConfig.addWatchTarget("./src/styles/");
-   eleventyConfig.addPassthroughCopy("./src/assets/");
+   eleventyConfig.addPassthroughCopy("/src/styles/");
+   eleventyConfig.addWatchTarget("/src/styles/");
+   eleventyConfig.addPassthroughCopy("/src/assets/");
+   eleventyConfig.addWatchTarget("/src/assets/");
    
    eleventyConfig.addFilter('readableDate', (dateObj) => {
      return DateTime.fromJSDate(dateObj, { zone: 'utc+9' }).toFormat(
